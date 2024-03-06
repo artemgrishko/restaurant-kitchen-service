923d409ac3d6abd1ecb455f1d94148175e4ff450
Artem Hryshko <artemggg2222@gmail.com>
2024-03-06 12:01:36 +0200

    add new path datail for dishes

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from .views import index, DishTypeListView, CookListView, DishListView, CookDetailView
+from .views import (
+    index,
+    DishTypeListView,
+    DishListView,
+    CookDetailView,
+    DishDetailView,
+)
 
 urlpatterns = [
     path("", index, name='index'),
@@ -9,7 +15,8 @@ urlpatterns = [
          name="dish_types-list"),
     path("cooks/", CookListView.as_view(), name="cooks-list"),
     path("cooks/<int:pk>/", CookDetailView.as_view(), name="cooks-detail"),
-    path("dishes/", DishListView.as_view(), name="dishes-list")
+    path("dishes/", DishListView.as_view(), name="dishes-list"),
+    path("dishes/<int:pk>/", DishDetailView.as_view(), name="dishes-detail"),
 ]
 
 app_name = "restaurant"
