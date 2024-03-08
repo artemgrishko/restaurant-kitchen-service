18d968bf45be1f48c7294c8e607e14a43f0506d6
Artem Hryshko <artemggg2222@gmail.com>
2024-03-08 18:20:15 +0200

    centered the text on the pages of updating

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from restaurant.models import Cook
 
 
-class CookCreationForm(forms.ModelForm):
+class CookCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Cook
         fields = UserCreationForm.Meta.fields + ("years_of_experience", "first_name", "last_name",)
